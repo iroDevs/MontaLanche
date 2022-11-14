ea85a941d3c51b214c23cc8528ba77944b749fb2
PedroDev <pedrohenriquelouresoliveira@gmail.com>
2022-11-13 21:34:07 -0300

    FEAT:fake info do back-end

--- front-end/src/pages/Home.jsx
+++ front-end/src/pages/Home.jsx
@@ -2,7 +2,33 @@ import React from 'react'
 import Login from '../components/Login'
 
 import './styles/Home.css'
-
+const fakeInfo = 
+[
+    {
+        nome:'X-caboco',
+        imagem:'',
+        preco:'X-egg',
+        ingredietes:['pao','bife','alface','tomate','miho','bacon']
+    },
+    {
+        nome:'X-bacon',
+        imagem:'',
+        preco:'X-egg',
+        ingredietes:['pao','bife','alface','tomate','miho','bacon']
+    },
+    {
+        nome:'X-caixa',
+        imagem:'',
+        preco:'X-egg',
+        ingredietes:['pao','bife','alface','tomate','miho','bacon']
+    },
+    {
+        nome:'X-toleto',
+        imagem:'',
+        preco:'X-egg',
+        ingredietes:['pao','bife','alface','tomate','miho','bacon']
+    },
+]
 function pages () {
   return (
     <>
